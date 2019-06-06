be7e8e83972e23485b9cab426e193a8c6eb94b39
Aleh Kahnovich <kotovi4.oleg@gmail.com>
2019-06-06 09:05:16 +0300

    initial commit

---
+++ MyCoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
