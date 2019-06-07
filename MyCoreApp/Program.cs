1d4cd5f2f736bcd63f366bf0057090f17c8e1320
Aleh Kahnovich <kotovi4.oleg@gmail.com>
2019-06-07 09:34:55 +0300

    Hello

--- MyCoreApp/Program.cs
+++ MyCoreApp/Program.cs
@@ -6,7 +6,7 @@ namespace MyCoreApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!11");
+            Console.WriteLine("Hello World!111");
         }
     }
 }
